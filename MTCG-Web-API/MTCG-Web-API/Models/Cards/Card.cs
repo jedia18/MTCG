b5c22b1f0746342a700b08923fab50ad8f7950fb
jedia18 <91239933+jedia18@users.noreply.github.com>
2023-01-07 13:49:59 +0100

    Abstract Class Card
    implementation of abstract class card

---
+++ MTCG-Web-API/MTCG-Web-API/Models/Cards/Card.cs
@@ -0,0 +1,29 @@
+﻿namespace MTCG_Web_API.Models.Cards
+{
+    /// <summary>
+    /// Making  spell cards as enum
+    /// </summary>
+    public enum ElementOfCard {Normal = 0, Water = 1, Fire = 2}
+
+    /// <summary>
+    /// Making different card types as enum
+    /// </summary>
+    public enum TypeOfCard {Spell = 0, Goblin = 1, Dragon = 2, Wizard = 3,
+        Ork = 4, Knight = 5, Kraken= 6, Elf = 7, Troll = 8}
+
+
+    public abstract class Card
+    {
+        protected float damage;
+        protected ElementOfCard element;
+        protected TypeOfCard type;
+
+        public float Damage { get; set; }
+        public ElementOfCard Element { get; set; }
+        public TypeOfCard Type { get; set; }
+
+        public abstract bool isProtectedVsMonster(TypeOfCard opponentType);
+
+        public abstract double damageEffectivenessCalculation(Card opponentCard);
+    }
+}
