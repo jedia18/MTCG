8e506997e9d351573f2de165a4e7e50ff96a31a5
Alireza Jedi Tehrani <aj.tehrani1984@gmail.com>
2023-01-20 13:19:36 +0100

    Delete all the program

--- MTCG-Web-API/MTCG-Web-API/Authentication/ApplicationUser.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace MTCG_Web_API.Authentication
-{
-    public class ApplicationUser : IdentityUser
-    {
-
-    }
-}
--- MTCG-Web-API/MTCG-Web-API/Models/Users/UserStats.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MTCG_Web_API.Models.Users
-{
-    public class UserStats
-    {
-        public int Elo { get; set; }
-        public int Wins { get; set; }
-        public int Losses { get; set; }
-    }
-}
